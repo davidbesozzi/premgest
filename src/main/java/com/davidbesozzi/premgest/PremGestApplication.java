8c0c8ee17e33e85f5b570c51891c95d85bf0da99
David Besozzi <david.besozzi.ch@gmail.com>
2018-08-31 17:02:26 +0200

    Initial Project Add

---
+++ src/main/java/com/davidbesozzi/premgest/PremGestApplication.java
@@ -0,0 +1,12 @@
+package com.davidbesozzi.premgest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PremGestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PremGestApplication.class, args);
+    }
+}
---
+++ src/test/java/com/davidbesozzi/premgest/PremGestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.davidbesozzi.premgest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PremGestApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
